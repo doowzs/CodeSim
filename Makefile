14b6b8ff40b039df96ae11be8b43a600a17dcf19
Tianyun Zhang <doowzs@qq.com>
2020-11-19 12:50:48 +0800

    fix LLVM include path for libTooling

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CXX = clang++
 CXXFLAGS = -ggdb3 -Wall -MMD -std=c++17 -I$(shell pwd)
 LDFLAGS = -lm -lclang -lclang-cpp -lLLVM
 ifeq ($(shell uname), Darwin)
+	CXXFLAGS += -DLLVM_PREFIX=/usr/local/opt/llvm
 	CXXFLAGS += -I/usr/local/opt/llvm/include
 	LDFLAGS += -L/usr/local/opt/llvm/lib
 endif
@@ -19,10 +20,13 @@ codesim: $(OBJS)
 	@echo "[LD] codesim"
 	@$(CXX) $(LDFLAGS) -o $@ $^
 
-.PHONY: all clean
+.PHONY: all test clean
 .DEFAULT_GOAL=all
 
 all: codesim
+
+test: all
+	@./codesim -v tests/P1764/a.cpp tests/P1764/b.cpp
 
 clean:
 	@$(RM) -r build codesim
--- langs/cpp11.cpp
+++ langs/cpp11.cpp
@@ -1,6 +1,10 @@
 #include "langs/cpp11.h"
 
+#include <clang/Tooling/Tooling.h>
+using namespace clang::tooling;
+
 #include <string>
+#include <vector>
 #include <fstream>
 #include <iostream>
 using namespace std;
@@ -11,7 +15,25 @@ size_t Cpp11::load_contents() {
   cerr << "Loading " << file << "." << endl;
   size_t length = load_raw_code();
   cerr << "Loaded " << file << "." << endl;
-  cerr << raw_code << endl;
+
+  const vector<string> cx_args = {
+    "-E", "-x", "c++", "-std=c++11", "-pedantic"
+#ifdef LLVM_PREFIX
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+    , "-I" TOSTRING(LLVM_PREFIX) "/include/c++/v1/"
+#undef TOSTRING
+#undef STRINGIFY
+#endif
+  };
+  auto unit = buildASTFromCodeWithArgs(raw_code, cx_args);
+  if (unit == nullptr) {
+    cerr << "Failed to build AST for " << file << "." << endl;
+    return 0;
+  }
+
+  cerr << "Built AST for " << file << "." << endl;
+
   return length;
 }
 
