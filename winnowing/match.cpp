dac66c24fd5a8870721bf4273ac3d9baa7a6c250
Tianyun Zhang <doowzs@qq.com>
2020-11-21 22:38:08 +0800

    calculate score with continuous matching

--- winnowing/match.cpp
+++ winnowing/match.cpp
@@ -5,15 +5,35 @@
 #include <vector>
 using namespace std;
 
+// Increase score based on length of sequent matchings.
 double Winnowing::match(const Winnowing *A, const Winnowing *B) {
-  size_t count = 0;
+  uint8_t strike = 0;
+  size_t score = 0;
+  size_t last_pos = 0;
+  vector<size_t> last_matches;
   for (auto [pos, hash] : A->hashes) {
-    if (not B->find(hash).empty()) {
-      ++count;
-      cerr << "Match hash [" << hash << "] on pos [" << pos << "] of A." << endl;
+    const auto matches = B->find(hash);
+    if (not matches.empty()) {
+      bool continuous = false;
+      for (auto &lm : last_matches) {
+        for (auto &cm : matches) {
+          if (cm - lm == pos - last_pos) {
+            continuous = true;
+            break;
+          }
+        }
+      }
+      if (not continuous) strike = 0;
+      score += 1U << (strike++);
+      last_pos = pos;
+      last_matches.clear();
+      copy(matches.begin(), matches.end(), back_inserter(last_matches));
     } else {
-      cerr << "NO Match [" << hash << "] on pos [" << pos << "] of A." << endl;
+      strike = 0;
+      last_pos = 0;
+      last_matches.clear();
     }
   }
-  return count;
+  double frac = static_cast<double>((A->size() + B->size()) * 4 - 3);
+  return static_cast<double>(score) / frac * 100; // to percentage
 }
