2f643ef929d828922a87840cbf7dfe3c1940d556
Tianyun Zhang <doowzs@qq.com>
2020-11-22 13:30:01 +0800

    update matching logic

--- winnowing/match.cpp
+++ winnowing/match.cpp
@@ -13,27 +13,21 @@ double Winnowing::match(const Winnowing *A, const Winnowing *B) {
   vector<size_t> last_matches;
   for (auto [pos, hash] : A->hashes) {
     const auto matches = B->find(hash);
-    if (not matches.empty()) {
-      bool continuous = false;
-      for (auto &lm : last_matches) {
-        for (auto &cm : matches) {
-          if (cm - lm == pos - last_pos) {
-            continuous = true;
-            break;
-          }
+    bool continuous = false;
+    for (auto &lm : last_matches) {
+      for (auto &cm : matches) {
+        if (cm - lm == pos - last_pos) {
+          continuous = true;
+          break;
         }
       }
-      if (not continuous) strike = 0;
-      score += 1U << (strike++);
-      last_pos = pos;
-      last_matches.clear();
-      copy(matches.begin(), matches.end(), back_inserter(last_matches));
-    } else {
-      strike = 0;
-      last_pos = 0;
-      last_matches.clear();
     }
+    if (not continuous) strike = 0;
+    score += matches.size() << (strike++);
+    last_pos = matches.empty() ? 0 : pos;
+    last_matches.clear();
+    copy(matches.begin(), matches.end(), back_inserter(last_matches));
   }
-  double frac = static_cast<double>((A->size() + B->size()) * 4 - 3);
+  double frac = static_cast<double>(A->size() + B->size()) * 4 - 3;
   return static_cast<double>(score) / frac * 100; // to percentage
 }
